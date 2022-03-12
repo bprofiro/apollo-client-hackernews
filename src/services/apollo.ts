3d0a0a119ad625a1862781d5056710e891148199
Brenda Profiro <brendaprofiro@gmail.com>
2022-03-12 12:39:02 -0300

    chore: Create apollo client integration

---
+++ src/services/apollo.ts
@@ -0,0 +1,46 @@
+import { ApolloClient, createHttpLink, InMemoryCache, split } from "@apollo/client";
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { setContext } from '@apollo/client/link/context';
+import { AUTH_TOKEN } from "../common/constants/auth";
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN)
+    }
+  }
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation }: any = getMainDefinition(query);
+    return (
+      kind === 'OperationDefinition' &&
+      operation === 'subscription'
+    );
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
+export const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+})
