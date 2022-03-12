f1275cf65168905f3f3728604c4871144eb20b6d
Brenda Profiro <brendaprofiro@gmail.com>
2022-03-12 11:36:53 -0300

    chore: Create Header component

---
+++ src/components/Header/Logo.tsx
@@ -0,0 +1,9 @@
+import { Text } from '@chakra-ui/react'
+
+export const Logo = () => {
+  return (
+    <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">
+      Hacker News
+    </Text>
+  )
+}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,39 @@
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react'
+import { RiMenuLine } from 'react-icons/ri'
+import { useSideBarDrawer } from '../../hooks/useSideBarDrawer'
+import { Logo } from './Logo'
+
+export const Header = () => {
+  const { onOpen } = useSideBarDrawer()
+
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true
+  })
+
+  return (
+    <Flex
+      as="header"
+      w="100%"
+      maxWidth={1480}
+      h="20"
+      mx="auto"
+      mt="4"
+      px="6"
+      align="center"
+    >
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Abrir navegação"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        />
+      )}
+
+      <Logo />
+    </Flex>
+  )
+}
